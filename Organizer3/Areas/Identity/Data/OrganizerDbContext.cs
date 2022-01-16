81154e3d1b72dde4c4dd9b720defb97a5331f74a
Marek Żyła <33495575+Samo276@users.noreply.github.com>
2022-01-16 19:38:06 +0100

    Initial commit

---
+++ Organizer3/Areas/Identity/Data/OrganizerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Organizer3.Areas.Identity.Data;
+
+namespace Organizer3.Data;
+
+public class OrganizerDbContext : IdentityDbContext<AppUser>
+{
+    public OrganizerDbContext(DbContextOptions<OrganizerDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
