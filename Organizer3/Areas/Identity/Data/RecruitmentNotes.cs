0c91d74210043d64ac1d4db4a6c1e9d212e36dff
Marek Żyła <33495575+Samo276@users.noreply.github.com>
2022-05-04 00:19:32 +0200

    dodano recruitment notes

---
+++ Organizer3/Areas/Identity/Data/RecruitmentNotes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Organizer3.Areas.Identity.Data
+{
+    public class RecruitmentNotes
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        [ForeignKey("Recruitment")]
+        public int ? RecruitmentId { get; set; }
+        public string ? NoteAuthor { get; set; }        
+        public string ? NoteContent { get; set; }
+    }
+}
